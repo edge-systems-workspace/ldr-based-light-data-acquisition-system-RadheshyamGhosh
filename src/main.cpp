1182a579a94989b3206062b3e720977d3452c79a
RadheshyamGhosh <shambhughosh843@gmail.com>
2026-02-20 09:25:28 +0530

    final commit and delay

--- src/main.cpp
+++ src/main.cpp
@@ -10,37 +10,23 @@
  * and displays structured output via Serial Monitor.
  */
 
- // TODO 1:
 #define LDR_PIN A0
 
 void setup() {
 
-    // TODO 2:
     Serial.begin(9600);
-
-    // TODO 3:
     while (!Serial);
 }
 
 void loop() {
 
-    // TODO 5:
     int ldrRaw = analogRead(LDR_PIN);
     int lightPercent = map(ldrRaw, 0, 1023, 0, 100);
 
-    // TODO 6:
     Serial.print("LDR Raw Value: ");
     Serial.print(ldrRaw);
     Serial.print(" | Light: ");
     Serial.print(lightPercent);
     Serial.println("%");
-
-    // TODO 7:
-    // Apply threshold logic (Bright / Dark detection)
-
-    // TODO 8:
-    // Print brightness status
-
-    // TODO 9:
-    // Add delay (500ms or 1 second)
+    delay(500);
 }
